6fab21c1a82920a465188a2f5379fc4cf2482856
Eugenio Tampieri <eugenio@eutampieri.eu>
2024-12-14 14:24:57 +0100

    Upload images using Rclone

--- tools/on_off_detection/src/main.rs
+++ tools/on_off_detection/src/main.rs
@@ -1,3 +1,45 @@
+use cv2::prelude::*;
+use opencv as cv2;
+
+const REGION: cv2::core::Rect = cv2::core::Rect::new(568, 276, 27, 87);
+const THRESHOLD: f64 = 220f64;
+
+fn is_on<M: cv2::core::ToInputArray>(image: M) -> bool {
+    // load image
+    let image = cv2::imgcodecs::imdecode(&image, cv2::imgcodecs::IMREAD_UNCHANGED).unwrap();
+
+    // convert to grey
+    let mut image_grey =
+        unsafe { cv2::core::Mat::new_size(image.size().unwrap(), cv2::core::CV_8UC3) }.unwrap();
+    cv2::imgproc::cvt_color(&image, &mut image_grey, cv2::imgproc::COLOR_BGR2GRAY, 0).unwrap();
+
+    // extract roi
+    let image_region = image_grey.roi(REGION).unwrap();
+
+    // binarise roi
+    let mut binarized_region =
+        unsafe { cv2::core::Mat::new_size(REGION.size(), cv2::core::CV_8UC3) }.unwrap();
+    cv2::imgproc::threshold(
+        &image_region,
+        &mut binarized_region,
+        THRESHOLD,
+        255f64,
+        cv2::imgproc::THRESH_BINARY,
+    )
+    .unwrap();
+
+    // calculate average pixel value
+    let (count, sum) = binarized_region
+        .iter::<u8>()
+        .unwrap()
+        .map(|x| x.1)
+        .fold((0usize, 0usize), |(count, sum), x| {
+            (count + 1, sum + x as usize)
+        });
+    let avg = sum as f32 / count as f32;
+    dbg!(avg) >= 0.85
+}
+
 fn main() {
     let frame = {
         let mut f = vec![];
@@ -9,5 +51,31 @@ fn main() {
             .unwrap();
         f
     };
-    dbg!(frame);
+    let timestamp = std::time::SystemTime::now()
+        .duration_since(std::time::SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    let filename = format!("{}.jpg", timestamp);
+    std::fs::write(&filename, &frame).unwrap();
+    let image = cv2::core::Mat::from_slice(&frame).unwrap();
+
+    let base_path = std::env::var("BASE_RCLONE_PATH").unwrap();
+
+    let dst_path = if is_on(&image) {
+        "/labeled/on"
+    } else {
+        "/labeled/off"
+    };
+    println!("Uploading {filename} to {dst_path}...");
+    let output = std::process::Command::new("rclone")
+        .arg("copy")
+        .arg(&filename)
+        .arg(format!("{base_path}{dst_path}"))
+        .output()
+        .unwrap();
+    println!("status: {}", output.status);
+    std::io::Write::write_all(&mut std::io::stdout(), &output.stdout).unwrap();
+    std::io::Write::write_all(&mut std::io::stderr(), &output.stderr).unwrap();
+    std::fs::remove_file(filename).unwrap();
 }
